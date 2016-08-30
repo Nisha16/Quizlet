8191a38b00c811f855bc20c569e0799259fafd16
Vidya <vidyags@outlook.com>
2016-08-30 15:05:14 -0700

    added ng-if directive, logic for quiz building

---
+++ js/controllers/quiz.js
@@ -0,0 +1,42 @@
+(function() {
+  angular
+  .module("quizlet")
+  .controller("quizCtrl", QuizController);
+
+  QuizController.$inject = ['quizMetrics','DataService'];
+  function QuizController(quizMetrics,DataService){
+    var vm = this;
+    vm.quizMetrics = quizMetrics;
+    vm.dataService = DataService;
+    vm.activeQuestion = 0;
+    vm.setActiveQuestion = setActiveQuestion;
+    vm.questionAnswered = questionAnswered;
+
+    var numQuestionAnswered = 0;
+
+    function setActiveQuestion() {
+      var breakOut = false;
+      var quizLength = DataService.quizQuestions.length - 1;
+
+      while(!breakOut){
+        vm.activeQuestion = vm.activeQuestion < quizLength? ++vm.activeQuestion:0;
+
+        if (DataService.quizQuestions[vm.activeQuestion].selected === null){
+          breakOut = true;
+        }
+      }
+    }
+
+    function questionAnswered() {
+
+      var quizLength = DataService.quizQuestions.lngth;
+      if(DataService.quizQuestions[vm.activeQuestion].selected !== null){
+        numQuestionAnswered++;
+        if(numQuestionAnswered >= quizLength){
+          //finalize the quiz
+        }
+      }
+      vm.setActiveQuestion();
+    }
+  }
+})();
