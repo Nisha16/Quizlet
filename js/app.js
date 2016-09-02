e1c0b824083652990f64352d8c39eff9b8b9e52d
Vidya <vidyags@outlook.com>
2016-09-02 15:43:39 -0700

    final code submission

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 (function() {
+    /*
+     * Declaration of main angular module for this appllication.
+     *
+     * It is named quizlet and has no dependencies (hence the
+     * empty array as the second argument)
+     */
 
     angular
     .module("quizlet", []);
